b7a77de9704bd3879c1e4574260570d06dd8a78e
James Threlfall <jwt104@googlemail.com>
2017-06-06 16:55:21 +0100

    fix bug where ekin can be below energy required by UE

--- src/core/r_main.f90
+++ src/core/r_main.f90
@@ -289,7 +289,7 @@ SUBROUTINE JTMUcalc(mu,USTART,GAMMASTART, Ekin,alpha,RSTART,T1,T2)
   REAL(num), INTENT(OUT) :: mu, gammastart, Ustart
   REAL(num), DIMENSION(3) :: B,El,a2,a3,a4,a5,a6,a7,a8,a9,a10,ue, RT
   !REAL(num) :: magB,vtot,vperp, vparstart!,Erest
-  REAL(num) :: modB,vtot, gamma
+  REAL(num) :: modB,vtot, gamma, Etemp
  
  !calculate B, E, V at this point/time:
  CALL FIELDS(RSTART,T1,El,B,a2,a3,a4,a5,a6,a7,a8,a9,a10,T1,T2)
@@ -308,18 +308,38 @@ SUBROUTINE JTMUcalc(mu,USTART,GAMMASTART, Ekin,alpha,RSTART,T1,T2)
  
  ! E X B drift
  ue=cross(El,B)/dot(B,B)  !*0.5
+ Etemp=Ekin
  
- gamma=Ekin/m/c/c*AQ+1.0d0
+ !need to check if 1/2mUE^2 is covered by the initial KE
+ IF (Ekin.lt.0.5d0*m*vscl*vscl*dot(ue,ue)*6.242e18) THEN
+  PRINT*, 'WARNING: raising Initial KE to account for local UE drift'
+  Etemp=0.5*m*vscl*vscl*dot(ue,ue)*6.242e18
+ ENDIF
+ 
+ gamma=Etemp/m/c/c*AQ+1.0d0
  
  ! vtot^2=vpar^2+vperp^2+UE^2!!, UE should NOT come out of Gamma!
  !vtot=c/gamma*sqrt(gamma*gamma-1)			! I *think* vtot is dimensional
  !vtot=sqrt(c*c/gamma/gamma*(gamma*gamma-1)-vscl*vscl*dot(ue,ue))
- vtot=sqrt(c*c-c/gamma*c/gamma-vscl*vscl*dot(ue,ue))			! THRELFALL ET AL 2015
+ !vtot=sqrt(c*c-c/gamma*c/gamma-vscl*vscl*dot(ue,ue))			! THRELFALL ET AL 2015
+ vtot=sqrt(c*c*(1.0d0-1.0d0/gamma/gamma)-vscl*vscl*dot(ue,ue))
  ! vtot=sqrt(c*c-c/gamma*c/gamma-vce*vce*dot(ue,ue))			! THRELFALL ET AL 2015
+ 
+ !print*, 'gamma ', gamma
+ !print*, 'uE^2:', vscl*vscl*dot(ue,ue)
+ !print*, 'c*c-c/gamma*c/gamma', c*c-c/gamma*c/gamma
+ !print*, 'c*c*(1.0d0-1.0d0/gamma/gamma)', c*c*(1.0d0-1.0d0/gamma/gamma)
+ !PRINT*, c*c-c/gamma*c/gamma-vscl*vscl*dot(ue,ue)
+ !print*, 'vtot ', vtot 
+ 
+ IF (vtot/=vtot) THEN
+  PRINT*, VTOT
+  PRINT*, 'Vtot NaN ENCOUNTERED'
+  STOP
+ ENDIF
  
  !Ustart=(c*sqrt(gamma*gamma-1))*cos(alpha)
  Ustart=gamma*vtot*cos(alpha)
-
  
  mu=0.5_num*m*vtot*vtot*sin(alpha)*sin(alpha)*gamma*gamma/modB		
  
